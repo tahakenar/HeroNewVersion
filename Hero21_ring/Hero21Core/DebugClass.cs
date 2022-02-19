1f132cd4b9b599818fcccb7945cc634f5d6afb14
Emir Taha Kenar <kenaremirtaha@gmail.com>
2022-02-19 20:22:14 +0300

    debug class

--- Hero21_ring/Hero21Core/DebugClass.cs
+++ Hero21_ring/Hero21Core/DebugClass.cs
@@ -1,9 +1,121 @@
-using Microsoft.SPOT;
+
 using System;
+using System.Threading;
+using Microsoft.SPOT;
+using System.Text;
+
+
+using CTRE.Phoenix;
+using CTRE.Phoenix.Controller;
+using CTRE.Phoenix.MotorControl.CAN;
+using CTRE.Phoenix.Motion;
+using CTRE.Phoenix.MotorControl;
 
 namespace Hero21Core
 {
     class DebugClass
     {
+
+        private static string finalMsg;
+        private static string motorInfo;
+        private static string encoderInfo;
+
+        public enum ArmDebugModes
+        {
+            position,
+            voltage
+        }
+
+        public enum specCases
+        {
+            mainLoop,
+            joystickControl,
+            emergencyStop,
+            resetSensors
+        }
+
+        public static void LogSpecCases(specCases mode){
+
+            switch (mode)
+            {
+                case specCases.mainLoop:
+                    finalMsg = "Main loop is running";
+                    break;
+                case specCases.joystickControl:
+                    finalMsg = "Joystick control mode is executing";        
+                    break;
+                case specCases.emergencyStop:
+                    finalMsg = "EMERGENCY STOP";
+                    break;
+                case specCases.resetSensors:
+                    finalMsg = "Reset sensors";
+                    break;
+                default:
+                    finalMsg = "";
+                    break;
+            }
+
+            finalMsg = PadLeft(finalMsg, 50, '_');
+            CheckAndSendMsg(finalMsg);
+
+        }
+
+
+        public static void LogMainLoop()
+        {
+            finalMsg = "Main loop is running";
+            finalMsg = PadLeft(finalMsg, 50, '_');
+            if (SerialCom._uartDebug.CanWrite)
+                SerialCom.WriteDebug(finalMsg);
+            Watchdog.Feed();
+        }
+
+        // mode = 0: position, mode = 1 voltage  ref.
+        public static void LogRoboticArmCommands(ArmDebugModes mode)
+        {
+            switch (mode)
+            {
+                case ArmDebugModes.position:
+                    finalMsg = "Positon commands: ";
+                    break;
+                case ArmDebugModes.voltage:
+                    finalMsg = "Voltage commands: ";
+                    break;
+                default:
+                    finalMsg = "";
+                    break;
+            }
+            
+
+            for (int i = 0;  i < RoboticArm.armMotorNum; i++)
+            {
+                motorInfo = SerialCom.armCommandsArray[i].ToString();
+                finalMsg += motorInfo;
+            }
+
+            finalMsg = PadLeft(finalMsg,50,'_');
+
+            CheckAndSendMsg(finalMsg);
+        }
+
+        
+
+
+        private static string PadLeft(string strToBePadded, int length, char paddingElement)
+        {
+            while (strToBePadded.Length < length)
+            {
+                strToBePadded = strToBePadded + paddingElement;
+            }
+            return strToBePadded;
+        }
+
+        private static void CheckAndSendMsg(string msg)
+        {
+            if (SerialCom._uartDebug.CanWrite)
+                SerialCom.WriteDebug(msg);
+            Watchdog.Feed();
+        }
+
     }
 }
